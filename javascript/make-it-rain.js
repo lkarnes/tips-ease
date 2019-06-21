04d9f7ddb0e5731fe90c7e77a54041be3e526a89
lex_node <gabriel.j.shapiro@gmail.com>
2019-06-21 17:16:35 -0400

    adds javascript components and functionality enabling a server's life story to appear when you tip that server

--- javascript/make-it-rain.js
+++ javascript/make-it-rain.js
@@ -1,31 +1,39 @@
-
-// class SupplementalContent {
-//     constructor(supplementalContentElement) {
-//         this.supplementalContent = supplementalContentElement;
-//     }
-//
-//     makeVisible() {
-//         this.supplementalContent.style.display = "flex";
-//     }
-//
-//     makeInvisible() {
-//         this.supplementalContent.style.display = "none";
-//     }
-// }
-
 class ServerLifeElement {
     constructor(serverLife) {
         this.serverLifeElement = serverLife;
         this.serverLifeStory = this.serverLifeElement.querySelector("p");
+        this.serverName = this.serverLifeElement.dataset.name;
         this.serverLifeElement.style.display = "none";
     }
 
+    makeVisible() {
+        serverLivesArray.forEach(serverLife => serverLife.makeInvisible());
+        this.serverLifeElement.style.display = "flex";
+    }
 
-
+    makeInvisible() {
+        this.serverLifeElement.style.display = "none";
+    }
 }
 
 
-
 let serverLives = document.querySelectorAll(".server-life-info");
-serverLives.forEach(serverLife => new ServerLifeElement(serverLife));
 
+let serverLivesArray = [];
+
+serverLives.forEach(serverLife => {
+    serverLivesArray.push(new ServerLifeElement(serverLife));
+});
+
+
+document.querySelector("#intro-section-container button").addEventListener("click", () => console.log('yo')/*OPEN UP A POP UP WINDOW TO SELECT SERVER TO TIP*/);
+
+document.querySelector("#intro-section-container button").addEventListener("click", () => {
+    let serverName = prompt("Please enter your server's name. You can pick Spiderman, Batman or Iron Man. Please use exact text.");
+    // document.querySelector(`.server-life-info[data-name="${serverName}`).makeVisible();
+    serverLivesArray.forEach(serverObject => {
+        if (serverObject.serverName = serverName) {
+            serverObject.makeVisible();
+        }
+    })
+});
