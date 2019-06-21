fc7be0f2f2c05f88137f0f28c08eb5137ad5cda9
lex_node <gabriel.j.shapiro@gmail.com>
2019-06-21 17:23:36 -0400

    fixes javascript glitch that resulted in only one server being selectable

--- javascript/make-it-rain.js
+++ javascript/make-it-rain.js
@@ -3,6 +3,7 @@ class ServerLifeElement {
         this.serverLifeElement = serverLife;
         this.serverLifeStory = this.serverLifeElement.querySelector("p");
         this.serverName = this.serverLifeElement.dataset.name;
+        console.log(this.serverName);
         this.serverLifeElement.style.display = "none";
     }
 
@@ -29,10 +30,10 @@ serverLives.forEach(serverLife => {
 document.querySelector("#intro-section-container button").addEventListener("click", () => console.log('yo')/*OPEN UP A POP UP WINDOW TO SELECT SERVER TO TIP*/);
 
 document.querySelector("#intro-section-container button").addEventListener("click", () => {
-    let serverName = prompt("Please enter your server's name. You can pick Spiderman, Batman or Iron Man. Please use exact text.");
+    let serverNameSelected = prompt("Please enter your server's name. You can pick Spiderman, Batman or Iron Man. Please use exact text.");
     // document.querySelector(`.server-life-info[data-name="${serverName}`).makeVisible();
     serverLivesArray.forEach(serverObject => {
-        if (serverObject.serverName = serverName) {
+        if (serverObject.serverName === serverNameSelected) {
             serverObject.makeVisible();
         }
     })
